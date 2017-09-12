17eed8a1162765db955f25016351ae25627fd4f3
Andre Macedo <dee.xd@hotmail.com>
2017-09-12 14:55:16 -0300

    Adding Insertion Sort code

---
+++ src/insertion_sort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void insertion_sort(int *arr, int size);
+void imprime_array(int *arr, int size);
+
+int main(){
+
+    int arr[] = { 2, 1, 8, 2, 6, 8, 9, 7 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Antes: ");
+    imprime_array(arr, size);
+    insertion_sort(arr, size);
+    printf("\nDepois: ");
+    imprime_array(arr, size);
+
+    printf("\n\nPress ENTER key to Continue\n");
+    getchar();
+
+    return 0;
+}
+
+void insertion_sort(int *arr, int size){
+    int i, j;
+    for(i=0; i<size; i++){
+        for(j=i; j>0; j--){
+            if(arr[j-1] > arr[j]){
+                int tmp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = tmp;
+            }else{
+                break;
+            }
+        }
+    }
+}
+
+void imprime_array(int *arr, int size){
+    int i;
+    for(i=0; i<size; i++)
+        printf("%d ", arr[i]);
+}
